73649b2a0a0631ce39705faa48c60841114b5d89
Thitat Auareesuksakul <flux@thitat.net>
2024-04-11 01:26:24 +0900

    chore: rename rust module to `_endec` for IDE compat
    https://www.maturin.rs/project_layout#import-rust-as-a-submodule-of-your-project

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@ mod exceptions {
 use pyo3::prelude::*;
 
 #[pymodule]
-fn endec(_py: Python, module: &Bound<'_, PyModule>) -> PyResult<()> {
+fn _endec(_py: Python, module: &Bound<'_, PyModule>) -> PyResult<()> {
     module.add_function(wrap_pyfunction!(encode::encode, module)?)?;
     module.add_function(wrap_pyfunction!(decode::decode, module)?)?;
     Ok(())
